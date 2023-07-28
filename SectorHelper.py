51b34f2b51db87d12a6d344e03a316b7094c0549
Nico Foth <nico@foth.dev>
2023-07-28 22:25:13 +0200

    Added a cache for getSector and getSectors

--- SectorHelper.py
+++ SectorHelper.py
@@ -3,6 +3,7 @@ import db_handler
 class SectorHelper:
 
     sector_size = 16
+    sector_cache = {}
 
     def createSector(self, x: int, y: int) -> None:
         """
@@ -27,10 +28,21 @@ class SectorHelper:
         - db_handler.Sector.Sector: The sector at the given coordinates.
         """
 
+        # Check if the sector is already in the cache
+        print(self.sector_cache.keys())
+        if f"{x},{y}" in self.sector_cache.keys():
+            return self.sector_cache[f"{x},{y}"]
+
+        # If the sector is not in the cache, retrieve it from the database
         sector = db_handler.DB_Handler.getSector(x, y)
+
+        # If the sector doesn't exist in the database, create it
         if sector is None:
             self.createSector(x, y)
             sector = db_handler.DB_Handler.getSector(x, y)
+        
+        # Add the sector to the cache and return it
+        self.sector_cache[f"{x},{y}"] = sector
         return sector
 
     def getSectors(self, sector_list: list) -> list:
@@ -44,7 +56,22 @@ class SectorHelper:
         - list: A list of sectors.
         """
 
-        return db_handler.DB_Handler.getSectors(sector_list)
+        sectors = []
+        to_retrieve = []
+        for sector in sector_list:
+            if sector in self.sector_cache:
+                sectors.append(self.sector_cache[sector])
+            else:
+                to_retrieve.append(sector)
+        
+        if len(to_retrieve) > 0:
+            new_sectors = db_handler.DB_Handler.getSectors(to_retrieve)
+        
+            for sector in new_sectors:
+                self.sector_cache[f"{sector.scoord_x},{sector.scoord_y}"] = sector
+                sectors.append(sector)
+
+        return sectors
 
     def drawPixel(self, x: int, y: int, color: tuple) -> None:
         """
@@ -55,7 +82,7 @@ class SectorHelper:
         - y (int): The y-coordinate of the pixel.
         - color (tuple): A tuple representing the RGB color of the pixel.
         """
-        
+
         scoord_x = x // self.sector_size
         scoord_y = y // self.sector_size
         sector = db_handler.DB_Handler.getSector(scoord_x, scoord_y)
